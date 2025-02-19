fc679a927642a001c86821f9c0cab5b33474860e
herbyvor <remtom4@gmail.com>
2025-02-20 00:17:59 +0100

    Player in cart on start

--- brackeys_2025_godot/Zones/Scripts/GameManager.cs
+++ brackeys_2025_godot/Zones/Scripts/GameManager.cs
@@ -6,33 +6,25 @@ using Godot.Collections;
 public partial class GameManager : Node
 {
     
-    private Zone _currentScene;
-    private int _currentSceneIndex = 0;
     [Export] private Array<PackedScene> _scenes = new Array<PackedScene>();
     [Export] private Vector3 _scenePosition = new Vector3(0, 0, 0); //remplacer tmtc
     [Export] private Cart Cart;
+    [Export] private CharacterBody3D _player;
+    
+    private int _currentCycle = 0;
+    private Zone _currentScene;
+    private int _currentSceneIndex = 0;
 
     public override void _Ready()
     {
         Cart.ArrivedSignal += _cartArrived;
         _loadScene(0);
+        _player.Reparent(Cart);
     }
 
     public override void _Process(double delta)
     {
         Cart.allowedToMove = _currentScene.IsComplete;
-        
-        if(Input.IsActionJustPressed("test"))
-        {
-            if (Cart._state == Cart.State.Moving)
-            {
-                Cart.Stop();
-            }
-            else if (Cart._state == Cart.State.Stopped)
-            {
-                Cart.Start();
-            }
-        }
     }
 
     private void _loadScene(int sceneIndex)
@@ -56,9 +48,14 @@ public partial class GameManager : Node
     
     private void _cartArrived()
     {
-        if(_currentSceneIndex == _scenes.Count - 1)
+        if (_currentSceneIndex == _scenes.Count - 1)
+        {
             _loadScene(0);
+            _currentCycle++;
+        }
         else
+        {
             _loadScene(_currentSceneIndex + 1);
+        }
     }
 }
