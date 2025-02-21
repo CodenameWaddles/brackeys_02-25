794efa3d6e9570b5f0fcbb6ec44999bd39d3ff5f
CodenameWaddles <clovis.oudart@wanadoo.fr>
2025-02-21 18:46:51 +0100

    light flicker sound

--- brackeys_2025_godot/Objects/Scripts/Light.cs
+++ brackeys_2025_godot/Objects/Scripts/Light.cs
@@ -4,12 +4,14 @@ using System;
 public partial class Light : OmniLight3D
 {
 	[Export] private Node3D _bulbNode;
+	[Export] private AudioStreamPlayer3D _audioPlayer;
+	[Export] private AudioStream _flickerSound;
 	[Export] private Vector3 _onColor = new(1, 1, 0.2f);
 	
 	private ShaderMaterial _shaderMaterial;
 	private Vector3 _offColor = new(0.05f, 0.05f, 0.05f);
 	private const int _maxFlickers = 5;
-
+	
 	public override void _Process(double delta) {
 		if(Input.IsActionJustPressed("ui_accept")) {
 			Flicker(false);
@@ -19,16 +21,19 @@ public partial class Light : OmniLight3D
 	public override void _Ready() {
 		MeshInstance3D child = (MeshInstance3D) _bulbNode.FindChild("Industrial lantern A_1");
 		_shaderMaterial = (ShaderMaterial) child.MaterialOverride;
+		_audioPlayer.Stream = _flickerSound;
 	}
 	
 	private void Activate() {
 		LightEnergy = 1;
+		_audioPlayer.Play();
 		if(_shaderMaterial == null) return;
 		_shaderMaterial.SetShaderParameter("color", _onColor);
 	}
 	
 	private void Deactivate() {
 		LightEnergy = 0;
+		_audioPlayer.Play();
 		if(_shaderMaterial == null) return;
 		_shaderMaterial.SetShaderParameter("color", _offColor);
 	}
