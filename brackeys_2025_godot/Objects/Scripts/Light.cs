35a0b605dd061c196a6600dc65bc36854fc36462
CodenameWaddles <clovis.oudart@wanadoo.fr>
2025-02-23 04:21:44 +0100

    optional light shadow for performance

--- brackeys_2025_godot/Objects/Scripts/Light.cs
+++ brackeys_2025_godot/Objects/Scripts/Light.cs
@@ -8,6 +8,7 @@ public partial class Light : OmniLight3D
 	[Export] private AudioStream _flickerSound;
 	[Export] private Vector3 _onColor = new(1, 1, 0.2f);
 	[Export] private bool _flickering = true;
+	[Export] private bool _shadow = true;
 	
 	private Vector3 _offColor = new(0.05f, 0.05f, 0.05f);
 	private const int _maxFlickers = 5;
@@ -16,6 +17,7 @@ public partial class Light : OmniLight3D
 	public override void _Ready() {
 		_mesh = (MeshInstance3D) _bulbNode.FindChild("Industrial lantern A_1");
 		_audioPlayer.Stream = _flickerSound;
+		ShadowEnabled = _shadow;
 	}
 
 	public override void _Process(double delta) {
